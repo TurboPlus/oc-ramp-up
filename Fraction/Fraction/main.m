0d6a7bd42fe2631b4dc26381c3536f5eb6907935
guoliquan <guoliquan_camerc@163.com>
2023-07-24 23:35:39 +0800

    feat(basic):最简单分数类

--- Fraction/Fraction/main.m
+++ Fraction/Fraction/main.m
@@ -7,10 +7,45 @@
 
 #import <Foundation/Foundation.h>
 
+@interface Fraction : NSObject
+
+-(void) print;
+-(void) setNumrator: (int) n;
+-(void) setDenominator: (int) d;
+
+@end
+
+@implementation Fraction
+{
+    int numerator;
+    int denominator;
+}
+
+-(void) print {
+    NSLog(@"%i/%i", numerator, denominator);
+}
+
+-(void) setNumrator: (int) n {
+    numerator = n;
+}
+
+-(void) setDenominator: (int) d {
+    denominator = d;
+}
+
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Fraction *myFraction;
+        myFraction = [Fraction alloc];
+        myFraction = [myFraction init];
+        
+        [myFraction setNumrator:1];
+        [myFraction setDenominator:3];
+        
+        NSLog(@"The value of myFraction is: ");
+        [myFraction print];
     }
     return 0;
 }
