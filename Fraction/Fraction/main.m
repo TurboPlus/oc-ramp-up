efe0411aea4fa3d76931b8926074938dd85679df
guoliquan <guoliquan_camerc@163.com>
2023-07-24 23:17:05 +0800

    oc-ramp-up init

---
+++ Fraction/Fraction/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Fraction
+//
+//  Created by 郭立权 on 2023/7/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
